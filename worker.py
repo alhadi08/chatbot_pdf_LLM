e8f6ac6b512c13850f053f4d667b52d3614f036a
alhadi08 <160561284+alhadi08@users.noreply.github.com>
2024-03-07 14:57:26 +0000

    konten

--- worker.py
+++ worker.py
@@ -8,6 +8,7 @@ from langchain.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain import OpenAI
 
 # Load environment variables
 load_dotenv()
@@ -22,9 +23,8 @@ llm_embeddings = None
 def init_llm():
     global llm, llm_embeddings
     # Initialize the language model with the OpenAI API key
-    api_key="YOUR API KEY"
-    # ---> TODO: write your code here <----
-    
+    api_key="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"
+    llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key)
     # Initialize the embeddings for the language model
     llm_embeddings = OpenAIEmbeddings(openai_api_key = api_key)
 
@@ -32,8 +32,7 @@ def init_llm():
 def process_document(document_path):
     global conversation_retrieval_chain, llm, llm_embeddings
     # Load the document
-    # ---> TODO: write your code here <---
-    
+    loader = PyPDFLoader(document_path)
     documents = loader.load()
     # Split the document into chunks
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -51,8 +50,7 @@ def process_prompt(prompt):
     global chat_history
     # Pass the prompt and the chat history to the conversation_retrieval_chain object
     result = conversation_retrieval_chain({"question": prompt, "chat_history": chat_history})
-    # ---> TODO: Append the prompt and the bot's response to the chat history <--
-
+    chat_history.append((prompt, result["answer"]))
     # Return the model's response
     return result['answer']
 
